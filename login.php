582f57c5f2e027e6ccd515a266f7c58e3a250e51
Mau <mssfkdkd@gmail.com>
2026-04-19 22:56:31 -0600

    main

---
+++ login.php
@@ -0,0 +1,15 @@
+<?php
+include("conexion.php");
+
+$correo = $_POST['correo'];
+$password = $_POST['password'];
+
+$sql = "SELECT * FROM usuarios WHERE correo='$correo' AND password='$password'";
+$resultado = $conexion->query($sql);
+
+if ($resultado->num_rows > 0) {
+    header("Location: index.html");
+} else {
+    echo "Correo o contraseña incorrectos";
+}
+?>
