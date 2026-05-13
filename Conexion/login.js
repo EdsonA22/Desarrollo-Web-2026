eaf23968e157284814ba277b29d5fb4fff2016c8
EdsonA22 <EdsonAza22@outlook.com>
2026-05-12 23:51:26 -0600

    Cambios esteticos

--- Conexion/login.js
+++ Conexion/login.js
@@ -1,40 +1,42 @@
-    import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
-    import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
-    import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
-<<<<<<< HEAD
-    //AQUI ESTAN LOS DATOS DEL FIREBASE QUE CREE
-=======
-    // ⚠️ AQUI ESTAN LOS DATOS DEL FIREBASE QUE CREE
->>>>>>> b67c19a9b5e508f0c13cfcd359bf303391bd790f
-    const firebaseConfig = {
-        apiKey: "AIzaSyBAj-0ii_ok1P50SkMLklKGC2oMuQoJd1o",
-        authDomain: "sinap-6dfb5.firebaseapp.com",
-        projectId: "sinap-6dfb5",
-    };
+//AQUI ESTAN LOS DATOS DEL FIREBASE QUE CREE
+const firebaseConfig = {
+  apiKey: "AIzaSyBAj-0ii_ok1P50SkMLklKGC2oMuQoJd1o",
+  authDomain: "sinap-6dfb5.firebaseapp.com",
+  projectId: "sinap-6dfb5",
+};
 
-    // Inicializar Firebase
-    const app = initializeApp(firebaseConfig);
-    const auth = getAuth(app);
-    const db = getFirestore(app);
+// Inicializar Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
 
-    // Login
-    const form = document.getElementById("loginForm");
+// Login
+const form = document.getElementById("loginForm");
 
-    form.addEventListener("submit", async (e) => {
-        e.preventDefault();
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
 
-        const correo = document.getElementById("correo").value;
-        const password = document.getElementById("password").value;
+  const correo = document.getElementById("correo").value;
+  const password = document.getElementById("password").value;
 
-        try {
-            const userCredential = await signInWithEmailAndPassword(auth, correo, password);
-            alert("Inicio de sesión exitoso");
+  try {
+    const userCredential = await signInWithEmailAndPassword(
+      auth,
+      correo,
+      password,
+    );
+    alert("Inicio de sesión exitoso");
 
-            // Redirigir
-            window.location.href = "index.html";
-
-        } catch (error) {
-            alert("Error: " + error.message);
-        }
-    });
+    // Redirigir
+    window.location.href = "index.html";
+  } catch (error) {
+    alert("Error: " + error.message);
+  }
+});
